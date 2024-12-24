4bb2a2e25f961d6f56a3695813dc19c6f8b32865
V3-Dump_Bot <girikhon2006@gmail.com>
2024-12-24 20:51:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const hideButtons = document.querySelectorAll(".hide");
+const queries = document.querySelectorAll(".query");
+
+hideButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const answer = queries[index].querySelector(".innerdiv");
+    const isHidden = answer.style.display === "none" || !answer.style.display;
+
+    hideButtons.forEach((btn, idx) => {
+      queries[idx].querySelector(".innerdiv").style.display = "none";
+      btn.style.backgroundImage = "url('/assets/images/icon-plus.svg')";
+    });
+
+    if (isHidden) {
+      answer.style.display = "block";
+      button.style.backgroundImage = "url('/assets/images/icon-minus.svg')";
+    } else {
+      answer.style.display = "none";
+      button.style.backgroundImage = "url('/assets/images/icon-plus.svg')";
+    }
+  });
+});
